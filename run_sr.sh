3487caed051e4e918e2d58cb5cb8f9500b50b1e1
Te-Yuan Huang <huangty@stanford.edu>
2013-01-16 23:09:12 +0000

    allow specify vhost num in run_sr.sh

--- run_sr.sh
+++ run_sr.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-./sr_solution -s ec2-54-245-137-131.us-west-2.compute.amazonaws.com -p 8888 -v vhost1 -r rtable.vhost1 -t 0
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 vhost_num"
+    exit
+fi
+
+./sr_solution -s ec2-54-245-163-146.us-west-2.compute.amazonaws.com -p 8888 -v vhost$1 -r rtable.vhost$1 -t 0
