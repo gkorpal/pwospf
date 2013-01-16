fd98f5568e30e3308a1e27aa8ea2797040a04dd8
Te-Yuan Huang <huangty@stanford.edu>
2013-01-16 23:12:11 +0000

    update run_sr.sh

--- run_sr.sh
+++ run_sr.sh
@@ -4,4 +4,4 @@ if [ $# -ne 1 ]; then
     exit
 fi
 
-./sr_solution -s ec2-54-245-163-146.us-west-2.compute.amazonaws.com -p 8888 -v vhost$1 -r rtable.vhost$1 -t 0
+./sr_solution -t 300 -v vhost$1 -r rtable.vhost$1 -s ec2-54-245-163-146.us-west-2.compute.amazonaws.com -p 8888
