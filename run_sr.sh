55b7ef86d2813fb8a359f7bb1f6d9bf5b2d988a1
Te-Yuan Huang <huangty@stanford.edu>
2013-01-17 02:15:18 +0000

    update ip config

--- run_sr.sh
+++ run_sr.sh
@@ -4,4 +4,5 @@ if [ $# -ne 1 ]; then
     exit
 fi
 
-./sr_solution -t 300 -v vhost$1 -r rtable.vhost$1 -s ec2-54-245-163-146.us-west-2.compute.amazonaws.com -p 8888
+#./sr_solution -t 300 -v vhost$1 -r rtable.vhost$1 -s ec2-54-245-163-146.us-west-2.compute.amazonaws.com -p 8888
+./sr -t 300 -v vhost$1 -r rtable.vhost$1 -s ec2-54-245-163-146.us-west-2.compute.amazonaws.com -p 8888 -l vhost$1.pcap
