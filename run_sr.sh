7b8a3a5ade3faca481b459e09274c37b7b469d19
Te-Yuan Huang <huangty@stanford.edu>
2013-01-19 03:53:45 +0000

    update run_sr.sh

--- run_sr.sh
+++ run_sr.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ $# -ne 1 ]; then
-    echo "Usage: $0 vhost_num"
+    echo "Usage: $0 mininet_machine_ip vhost_num"
     exit
 fi
 
 #TODO: change the hostname into the ip/hostname of the machine runs mininet/pox
-./sr_solution -t 300 -v vhost$1 -r rtable.vhost$1 -s ec2-54-245-163-146.us-west-2.compute.amazonaws.com -p 8888 -l vhost$1.pcap
+./sr_solution -t 300 -v vhost$2 -r rtable.vhost$2 -s $1 -p 8888 -l vhost$2.pcap
